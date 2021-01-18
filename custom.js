8cba77bf3fd1afbc25b4a1f8f4204e8aaa3782b1
Plenkovic <visko@arsfutura.co>
2021-01-18 16:02:48 +0100

    first-commit

---
+++ custom.js
@@ -0,0 +1,19 @@
+function checkIfToManyIndustries() {
+  if($('.industry[type="checkbox"]:checked').length > 5) {
+      $('.industry_error_message').addClass('show');
+      $('#submit_button').addClass("industry-disabled")
+  } else {
+      $('#submit_button').removeClass("industry-disabled")
+      $('.industry_error_message').removeClass('show');
+  }
+}
+
+jQuery(function() {
+  checkIfToManyIndustries();
+
+  $(document).on('change', '.industry', function(event){
+      checkIfToManyIndustries();
+  })
+})
+
+~
